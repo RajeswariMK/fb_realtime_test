64b6d49018e3e856eeb4b268b37d37f483975034
RajeswariMK <rajeswarik@spritle.com>
2013-02-25 18:03:34 +0530

    Order update based on time

--- app/controllers/facebook_realtime_updates_controller.rb
+++ app/controllers/facebook_realtime_updates_controller.rb
@@ -32,7 +32,7 @@ class FacebookRealtimeUpdatesController < ApplicationController
   end
 
   def updates
-    @updates = FacebookLikes.find(:all)
+    @updates = FacebookLikes.find(:all, :order => "desc")
 
   end
 end
