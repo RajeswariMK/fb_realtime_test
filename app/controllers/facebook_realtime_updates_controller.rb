7bf7f8838b240c50b1ebd1061cd27fc494d7a44d
RajeswariMK <rajeswarik@spritle.com>
2013-02-25 16:42:17 +0530

    render text added

--- app/controllers/facebook_realtime_updates_controller.rb
+++ app/controllers/facebook_realtime_updates_controller.rb
@@ -14,17 +14,18 @@ class FacebookRealtimeUpdatesController < ApplicationController
           render :text => 'Failed to authorize facebook challenge request'
         end
     elsif request.method == "POST"
-        @updated_obj = JSON.parse(request.body.read)
-        facebook_like = FacebookLikes.new
-          @updated_obj["entry"].each do |entry|
-                                           
-                facebook_like.uid = entry["uid"]
-                facebook_like.facebook_id = entry["id"]
-                facebook_like.time = entry["time"]
-                facebook_like.changed_fields = entry["changed_fields"]
-                facebook_like.save
-          end
-          p facebook_like, "------------------------------------------"
+      @updated_obj = JSON.parse(request.body.read)
+      facebook_like = FacebookLikes.new
+      @updated_obj["entry"].each do |entry|
+                                       
+            facebook_like.uid = entry["uid"]
+            facebook_like.facebook_id = entry["id"]
+            facebook_like.time = entry["time"]
+            facebook_like.changed_fields = entry["changed_fields"]
+            facebook_like.save
+      end
+      p facebook_like, "------------------------------------------"
+      render :text => "Thanks for the update"
     end
   end
 end
