387ae43040316d500aa208ddb71bb75e231f526c
RajeswariMK <rajeswarik@spritle.com>
2013-02-27 13:14:42 +0530

    get first user token

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ActiveRecord::Base
   attr_accessible :caption, :comments_count, :created_time, :description, :facebook_infos_id, :fb_post_id, :fb_updated_time, :likes_count, :link_url, :message, :name, :picture_url, :post_type, :source
 
   def self.facebook_post
-  	current_user ||= User.find(session[:user_id]) if session[:user_id]
+  	current_user ||= User.first
   	@graph = Koala::Facebook::API.new(current_user.oauth_token)
   	@post = @graph.get_connections("me", "feed") 
   	p "executing method"  
