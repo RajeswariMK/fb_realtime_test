52703086cbf62f22e8c39b2221dc1535dc70fc76
RajeswariMK <rajeswarik@spritle.com>
2013-02-27 13:03:01 +0530

    Get user feed in Background job

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,11 @@ class Post < ActiveRecord::Base
   attr_accessible :caption, :comments_count, :created_time, :description, :facebook_infos_id, :fb_post_id, :fb_updated_time, :likes_count, :link_url, :message, :name, :picture_url, :post_type, :source
 
   def self.facebook_post
-  	p "executing method"  	
+  	 @graph = Koala::Facebook::API.new(current_user.oauth_token)
+  	@post = @graph.get_connections("me", "feed") 
+  	p "executing method"  
+   p @post , "post"
   end 
+
 end
 	
