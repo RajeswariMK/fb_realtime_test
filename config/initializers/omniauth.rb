b2a19edd2e64ff58eb1adf730c0c7c1072b5a5ad
RajeswariMK <rajeswarik@spritle.com>
2013-02-27 15:04:33 +0530

    update fb credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,14 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FB.APP_ID, FB.SECRETE
+  provider :facebook, FB.app_id, FB.secrete
 end
 
 module FB
-	APP_ID = '479510062109507'
-	SECRETE = '239049adeb477f2bd4c46d344cd1033b'
+	def self.app_id
+'479510062109507'
+end
+def self.secrete
+	'239049adeb477f2bd4c46d344cd1033b'
+end
 end
