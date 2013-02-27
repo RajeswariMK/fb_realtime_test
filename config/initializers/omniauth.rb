53488e9b008baa12ff6018eea41c28fb2f314c7d
RajeswariMK <rajeswarik@spritle.com>
2013-02-27 15:01:10 +0530

    All FB module and maintain app id and secrete

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,10 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '141213822708267', 'f9b95f55474f790335c44f4be592ec26'
+  provider :facebook, FB.APP_ID, FB.SECRETE
+end
+
+module FB
+	APP_ID = '479510062109507'
+	SECRETE = '239049adeb477f2bd4c46d344cd1033b'
 end
